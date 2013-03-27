0f78ef5f94a4defc727e000f301c9649d4ca8470
Marcelo Mogami <marcelo.mogami@imamt.com.br>
2013-03-27 14:52:17 -0400

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adendo::Application.config.secret_token = '491d16f84383257c0c78838145b83402d09865fc4e3fa439b33434de6a1b8f26524075ac4d96a9cd48a39bbc888eae6f3c348672ead62d1d8c11661f06414cfd'
